634cb3bee5a8495eff4e2dab8164955e1f545739
Craig Banach <craigsb93@gmail.com>
2016-03-29 13:19:07 +0000

    Completed Project

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+var regex = new RegExp(/[()]/g);
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.get("/", function(req, res) {
+    var IPReq = req.ip.split(":");
+    var IP = IPReq[IPReq.length - 1];
+    //console.log(req.useragent);
+    var browserOS = req.headers['user-agent'].split(regex);
+    res.end(JSON.stringify({
+        "ipaddress": IP, 
+        "language":req.acceptsLanguages()[0],
+        "software": browserOS[1]
+    }));
+})
 
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
+var server = app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
